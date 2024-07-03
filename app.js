4a455b9c86b7b9b71d9caa21f5cefee91ee9d109
hy2n <25565k@daum.net>
2024-07-03 17:11:55 +0900

    add client

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname,'/static')));
+
+app.get("/hello",(req,res) =>{
+    return res.status(400).send("Unknown");
+});
+
+app.listen(8080,() =>{
+    console.log("[INFO] Express Start");
+});
