77dc2f257ea8949019a7610fda89c39b732645a7
hy2n <25565k@daum.net>
2024-07-03 18:02:37 +0900

    client page edit

--- app.js
+++ app.js
@@ -3,17 +3,18 @@ const app = express();
 const path = require("path");
 const cors = require('cors');
 
-app.use(cors({
-    origin: 'http://auth.nanu.cc'
-  }));
-  
+app.use(cors());
 
-app.use(express.static(path.join(__dirname,'/static')));
+app.use(express.static(path.join(__dirname, '/static')));
 
-app.get("/hello",(req,res) =>{
+app.get("/hello", (req, res) => {
     return res.status(400).send("Unknown");
 });
 
-app.listen(8080,() =>{
+app.get("/gateway", (req, res) => {
+    return res.status(200).send("Unknown");
+});
+
+app.listen(8080, () => {
     console.log("[INFO] Express Start");
 });
