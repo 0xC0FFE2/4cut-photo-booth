d307ce31f5e930eceb4c888956d3c5f7cb194f29
hy2n <25565k@daum.net>
2024-07-06 20:06:22 +0900

    사진 출력 렌더링 로직 추가

--- routes/render.js
+++ routes/render.js
@@ -2,17 +2,27 @@ const express = require("express");
 const ejs = require('ejs');
 const router = express.Router();
 
-//File Upload 받기
-router.get("/:id", (req, res) => {
+
+router.get("/png/:id", (req, res) => {
     if (!req.params.id) {
         return res.status(404).json(({ status: 'failed', message: 'no id' }));
     }
-    res.render('.././render/4cut', 
+    res.render('.././render/4cutImage', 
         {
-            frame_url: '/assets/frames/dsm.png'
-
+            frame_url: 'love.png',
+            imgid:'84725-3'
         });
 });
 
+router.get("/mp4/:id", (req, res) => {
+    if (!req.params.id) {
+        return res.status(404).json(({ status: 'failed', message: 'no id' }));
+    }
+    res.render('.././render/4cutVideo', 
+        {
+            frame_url: 'love.png',
+            imgid:'84725-3'
+        });
+});
 
 module.exports = router;
